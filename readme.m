c0384574a1135f1809d57e8fb0f6d192454faddd
SamiHagrai <45062174+SamiHagrai@users.noreply.github.com>
2018-12-05 23:56:31 +0100

    Add files via upload

---
+++ readme.m
@@ -0,0 +1,40 @@
+% Unzip all files in Matlab current directory, then type "handrec"
+% on Matlab command window. A simple and intuitive GUI should appear.
+% 
+% GUI FUNCTIONS
+% 
+% SELECT IMAGE
+% Read the input image
+%
+% ADD SELECTED IMAGE TO DATABASE
+% Input image is added to database and will be used for training. An
+% integer ID is required. ID is a progressive integer number associated to
+% hand.
+%
+% DATABASE INFO
+% Show informations about the images present in database.
+%
+% HAND GEOMETRY RECOGNITION
+% Hand matching. Selected input image is compared with all hand images
+% present in database.
+%
+% DELETE DATABASE
+% Remove database from the current directory.
+%
+% INFO
+% Show informations about this software.
+%
+% FEATURE VISUALIZATION
+% Visualization utility.
+%
+% SOURCE CODE FOR HAND GEOMETRY RECOGNITION SYSTEM
+% How to obtain the complete source code.
+%
+% EXIT
+% Quit program.
+%
+% 
+% HAND GEOMETRY RECOGNITION SYSTEM V3
+% 
+%
+% For any question please email me (Sami_Hagrai-88@matlab-recognition-code.com)
---
+++ sourcecode.m
@@ -0,0 +1,7 @@
+% 
+% HAND GEOMETRY RECOGNITION SYSTEM V3
+%
+% In order to obtain the complete source code please visit
+% 
+% For any question please email me (Sami_Hagrai-88@matlab-recognition-code.com)
+
